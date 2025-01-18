01d0ce919cc120003c609e08cc9b7633e32ed2da
Rodrigo Correia <rodrigodx52@gmail.com>
2025-01-18 14:26:32 -0300

    criado o form de cadastro de usuario

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,5 +1,37 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import connection.SingleConnection;
+import model.User;
+
 public class UserDAO {
+	
+	private Connection connection;
+	
+	public UserDAO() {
+		connection = SingleConnection.getConnection();
+	}
+	
+	public void insert(User user) {
+		String sql = "insert into love_pets.users (name, email, password) values(?, ?, ?)";
+		
+		try {
+			PreparedStatement insert = connection.prepareStatement(sql);
+			
+			insert.setString(1, user.getName());
+			insert.setString(2, user.getEmail());
+			insert.setString(3, user.getPassword());
+			
+			insert.execute();
+			connection.commit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
